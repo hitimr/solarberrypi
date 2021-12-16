c01d3d8cd743a58ed79769af177e746381254420
hitimr <hitimr@gmail.com>
2021-12-16 19:20:43 +0000

    implemented full cycle

--- src/Display_Interface.py
+++ src/Display_Interface.py
@@ -3,7 +3,9 @@ import os
 import time
 import traceback
 from pathlib import Path
-import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import pandas as pd
 
 # Project Stuff
 import os,sys,inspect
@@ -21,41 +23,66 @@ import pandas
 from lib.epd7in5_V2 import *
 from PIL import Image,ImageDraw,ImageFont
 
+def generate_plot(data, outFileName):
+    df = data.copy()
+    df = df / 1000 # set to KWh
+    df = df.iloc[1: , :]    # Drop first row
+
+    # Plot lines
+    scaling = 100
+    fontsize = 14
+    width = EPD_WIDTH / scaling
+    height = EPD_HEIGHT / scaling
+    cmap = colors.ListedColormap(["black"])
+
+    # Add total consumption
+    hours = pd.Timedelta(data.index.values[-1] - data.index.values[0]) / pd.Timedelta("1 hour")
+    total_production = df["Production"].sum() / hours
+    total_consumption = df["Consumption"].sum() / hours
+    total = (total_consumption - total_production) 
+
+    df.plot(style=["--", "-"], figsize=(width, height), cmap=cmap, fontsize=fontsize)
+
+    # Formatting
+    plt.title(f"Gesamtverbrauch (24h): {total:.2f}kWh", fontsize=fontsize*1.2)
+    plt.grid(linewidth=2)
+    plt.xlabel("")
+    plt.ylabel("Leistung [kW]", fontsize=fontsize)
+    plt.legend(labels=[f"Produktion: {total_production:.2f} kWh", f"Verbrauch:  {total_consumption:.2f} kWh"], fontsize=fontsize)
+    plt.gca().xaxis.set_tick_params(which='both', width=3)
+    plt.gca().yaxis.set_tick_params(which='both', width=3)
 
 
-class Display_Interface:
-    self.width = int
-    self.height = int
+    # Output
+    page_margin = 0.075
+    plt.subplots_adjust(left=0.06, right=0.995, top=0.995, bottom=0.1)
+    plt.savefig(outFileName, dpi=500, facecolor="white")
 
-    def __init__(self, width=cfg.EPD_WIDTH, height=cfg.EPD_HEIGHT):
-        # class init
-        self.width = int(width)
-        self.heigth = int(height)
 
-        # Init hardware
-        self.epd = EPD()
-        self.epd.init()
-        self.epd.Clear()
-    
-    def display_image(self, fileName):
-        # load image
-        img = Image.open(fileName)
 
-        # transform to binary array
-        thresh =  cfg.BW_THRESH
-        fn = lambda x : 255 if x > thresh else 0
-        r = img.convert('L').point(fn, mode='1')
+def display_image(fileName):
+    epd = EPD()
+    epd.init()
 
-        # resize to fit display
-        img = img.resize((self.width, self.height))
-        #file_out = DIR_OUT + "plot.bmp"
-        #img.save(file_out)
+    # load image
+    img = Image.open(fileName)
 
-        Himage = Image.open(img)
-        self.epd.display(epd.getbuffer(Himage))
-        time.sleep(1)
+    # transform to binary array
+    thresh =  cfg.BW_THRESH
+    fn = lambda x : 255 if x > thresh else 0
+    r = img.convert('L').point(fn, mode='1')
+
+    # resize to fit display
+    img = img.resize((cfg.EPD_WIDTH, cfg.EPD_HEIGHT))
+
+    # write to display
+    epd.display(epd.getbuffer(img))
+
+    # save output to file as well
+    file_out = cfg.DIR_OUT + "plot.bmp"
+    img.save(file_out)
+
 
 
 if __name__ == "__main__":
-    di = Display_Interface()
-    di.display_image("../out/plot.png")
+    display_image(cfg.DIR_OUT + "plot.png")
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from logging.handlers import TimedRotatingFileHandler
 
 from config import *
 from SE_Interface import *
+from Display_Interface import *
 
 
 if __name__ == "__main__":
@@ -12,10 +13,14 @@ if __name__ == "__main__":
     logfilename = LOGGING_BASE_FILENAME + "_" + now.strftime("%y%m%d")
     logging.basicConfig(filename=LOGGING_BASE_FILENAME, filemode="a", encoding='utf-8', level=logging.DEBUG)
 
-    logging.info("Initialising SE Interface")
+
     se_interface = SE_Interface()
+    data = se_interface.request_SitePowerDetailed(datetime.now(), timedelta(days=1))
+
+    plotFile = DIR_OUT + "plot.png"
+    generate_plot(data, plotFile)
+    display_image(plotFile)
 
 
-    #data = se_interface.request_SitePowerDetailed(datetime.now(), timedelta(days=1))
-    #assert
+    
 
