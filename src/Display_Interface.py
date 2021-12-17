a7c17504112e8810e676ef01b04202076098130b
hitimr <hitimr@gmail.com>
2021-12-18 00:12:51 +0100

    updated plotting

--- src/Display_Interface.py
+++ src/Display_Interface.py
@@ -25,6 +25,7 @@ from PIL import Image,ImageDraw,ImageFont
 
 def generate_plot(data, outFileName):
     df = data.copy()
+    df = df[["Production", "Consumption", "FeedIn",]]
     df = df / 1000 # set to KWh
     df = df.iloc[1: , :]    # Drop first row
 
@@ -35,28 +36,43 @@ def generate_plot(data, outFileName):
     height = EPD_HEIGHT / scaling
     cmap = colors.ListedColormap(["black"])
 
+
     # Add total consumption
     hours = pd.Timedelta(data.index.values[-1] - data.index.values[0]) / pd.Timedelta("1 hour")
     total_production = df["Production"].sum() / hours
     total_consumption = df["Consumption"].sum() / hours
-    total = (total_consumption - total_production) 
+    total_feedin = df["FeedIn"].sum() / hours
+    total = (total_consumption - total_production - total_feedin) 
 
-    df.plot(style=["--", "-"], figsize=(width, height), cmap=cmap, fontsize=fontsize)
+
+    df.plot(
+        style=["--", "-", ":"], 
+        figsize=(width, height), 
+        cmap=cmap, 
+        fontsize=fontsize,
+        )
+
 
     # Formatting
+    for line in plt.gca().get_lines(): line.set_linewidth(2)  # Line thickness
     plt.title(f"Gesamtverbrauch (24h): {total:.2f}kWh", fontsize=fontsize*1.2)
     plt.grid(linewidth=2)
     plt.xlabel("")
     plt.ylabel("Leistung [kW]", fontsize=fontsize)
-    plt.legend(labels=[f"Produktion: {total_production:.2f} kWh", f"Verbrauch:  {total_consumption:.2f} kWh"], fontsize=fontsize)
+    plt.legend(
+        labels=[
+            f"Produktion: {total_production:.2f} kWh", 
+            f"Verbrauch:  {total_consumption:.2f} kWh",
+            f"Rückspeis.: {total_feedin:.2f} kWh",], 
+        fontsize=fontsize)
     plt.gca().xaxis.set_tick_params(which='both', width=3)
     plt.gca().yaxis.set_tick_params(which='both', width=3)
 
-
     # Output
+    #plt.tight_layout()
     page_margin = 0.075
     plt.subplots_adjust(left=0.06, right=0.995, top=0.995, bottom=0.1)
-    plt.savefig(outFileName, dpi=500, facecolor="white")
+    plt.savefig("../out/plot.png", dpi=500, facecolor="white")
 
 
 
