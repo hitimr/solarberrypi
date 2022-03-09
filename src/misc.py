c93d22cf15a16dd845e51473d17cd6ae47bf32cb
hitimr <hitimr@gmail.com>
2022-03-09 09:53:56 +0100

    birthday extension

--- src/misc.py
+++ src/misc.py
@@ -12,6 +12,11 @@ def is_birthday(date):
     if(date.month == 3 and date.day == 9):
         return True
 
+    # 2022 she was on holiday on her birthday
+    # she asked to extend by 1 day
+    if((date.month == 3) and (date.day == 10) and (date.year == 2022)):
+        return True
+
     # Papa
     if(date.month == 9 and date.day == 25):
         return True
